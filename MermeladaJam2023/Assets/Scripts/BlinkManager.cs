d60a18ccbbb7936d5b5bd310fa4c246767138a69
jmguillenfernandez <61391449+jmguillenfernandez@users.noreply.github.com>
2023-11-17 21:38:50 +0100

    Blink

---
+++ MermeladaJam2023/Assets/Scripts/BlinkManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkManager : MonoBehaviour
+{
+    private Animator animator;
+
+    public float blinkTime=0.5f;
+
+    [HideInInspector]
+    public bool isEyeClosed = false;
+
+    public bool DEBUG_TEST_BLINK = false;
+
+    private void Awake()
+    {
+            animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(DEBUG_TEST_BLINK)
+        {
+            DEBUG_TEST_BLINK=false;
+            StartBlink();
+        }
+    }
+
+    public void StartBlink()
+    {
+        animator.SetTrigger("Close");
+        StartCoroutine(HoldBlink());
+    }
+
+    IEnumerator HoldBlink()
+    {
+        isEyeClosed = true;
+        yield return new WaitForSeconds(blinkTime);
+        animator.SetTrigger("Open");
+        isEyeClosed=false;
+    }
+
+}
