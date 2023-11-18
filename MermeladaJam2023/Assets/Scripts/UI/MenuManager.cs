f4a1b934f73162f3425cf573ac89cb51aaeb4f71
jmguillenfernandez <61391449+jmguillenfernandez@users.noreply.github.com>
2023-11-18 19:44:40 +0100

    Options Menu

--- MermeladaJam2023/Assets/Scripts/UI/MenuManager.cs
+++ MermeladaJam2023/Assets/Scripts/UI/MenuManager.cs
@@ -1,10 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        if(resolutionDropdown != null)
+        {
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+
+            for(int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex= i;
+                }
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
@@ -17,6 +55,21 @@ public class MenuManager : MonoBehaviour
 
     public void SetVolume(float volume)
     {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
 
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
