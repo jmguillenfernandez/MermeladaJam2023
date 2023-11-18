730f520c69eb349ced0e91629acc97f12428249a
jmguillenfernandez <61391449+jmguillenfernandez@users.noreply.github.com>
2023-11-18 10:54:47 +0100

    Enemy Moves when unseen

--- MermeladaJam2023/Assets/Scripts/Monster/MonsterMovementTest.cs
+++ MermeladaJam2023/Assets/Scripts/Monster/MonsterMovementTest.cs
@@ -6,20 +6,55 @@ using UnityEngine.AI;
 public class MonsterMovementTest : MonoBehaviour
 {
     public GameObject player;
-    
+
+    public Camera cam;
+
     [HideInInspector]
     public NavMeshAgent agent;
+    Collider col;
+    Ray ray;
+    RaycastHit hit;
+    float dist;
 
+    [Header("DEBUG")]
     public bool slipperyMonster = false;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        col = GetComponent<Collider>();
+    }
+
+    private bool IsVisible()
+    {
+        var bounds = col.bounds;
+        if(GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(cam), bounds))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private bool IsHiddenByObject()
+    {
+        ray = new Ray(cam.transform.position, transform.position - cam.transform.position);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider ==gameObject.GetComponent<Collider>())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     void Update()
     {
-        if(player.GetComponentInChildren<BlinkManager>().isEyeClosed)
+        IsHiddenByObject();
+        bool eyeState = player.GetComponentInChildren<BlinkManager>().isEyeClosed;
+
+        if (IsVisible() || eyeState || IsHiddenByObject())
         {
             agent.isStopped = false;
             agent.SetDestination(player.transform.position);
